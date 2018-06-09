f9583b0c581286ccb6100da366d033c4ce6e4847
David Royer <droyer01@gmail.com>
2018-06-09 15:32:15 -0400

    added purgeCss whitelistpatterns to fix removed styling issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,37 +7,59 @@ class TailwindExtractor {
     return content.match(/[A-z0-9-:/]+/g) || []
   }
 }
+const purgecssWhitelistPatterns = [
+  // /^elp(_.*)?$/,
+  /^page-/,
+  /^nuxt-/
+]
 
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: "tailwind-documentation",
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "Nuxt.js project" }
+    meta: [{
+        charset: "utf-8"
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
+      },
+      {
+        hid: "description",
+        name: "description",
+        content: "Nuxt.js project"
+      }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{
+      rel: "icon",
+      type: "image/x-icon",
+      href: "/favicon.ico"
+    }],
     bodyAttrs: {
       class: "font-sans leading-normal"
     }
   },
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: "#9561e2" },
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: "#9561e2"
+  },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     extractCSS: true,
     postcss: [
       require('tailwindcss')('./tailwind.js'),
       require('autoprefixer')
     ],
-    extend(config, { isDev, isClient }) {
+    extend(config, {
+      isDev,
+      isClient
+    }) {
       if (!isDev) {
         config.plugins.push(
           new PurgecssPlugin({
@@ -48,13 +70,12 @@ module.exports = {
               path.join(__dirname, './layouts/**/*.vue'),
               path.join(__dirname, './components/**/*.vue')
             ]),
-            extractors: [
-              {
-                extractor: TailwindExtractor,
-                extensions: ['vue']
-              }
-            ],
-            whitelist: ['html', 'body', 'nuxt-progress', 'page-transition']
+            extractors: [{
+              extractor: TailwindExtractor,
+              extensions: ['vue']
+            }],
+            whitelist: ['html', 'body', 'nuxt-progress', 'page-enter', 'page-enter-active', 'page-leave', 'page-leave-active'],
+            whitelistPatterns: purgecssWhitelistPatterns
           })
         )
       }
