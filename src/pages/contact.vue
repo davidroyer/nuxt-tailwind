fa5d9c11f5010cd084783e4d300f18f715577977
David Royer <droyer01@gmail.com>
2018-06-20 05:22:21 -0400

    update

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -4,27 +4,21 @@
   <div class="w-full max-w-md mx-auto">
     <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
       <div class="mb-4">
-        <label class="block text-grey-darker text-sm font-bold mb-2" for="firstName">
-            Name
-          </label>
+        <label class="block text-grey-darker text-sm font-bold mb-2" for="firstName">Name</label>
         <input v-model="form.name" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight" id="firstName" type="text" placeholder="Your First Name">
       </div>
       <div class="mb-4">
-        <label class="block text-grey-darker text-sm font-bold mb-2" for="email">
-            Email
-          </label>
+        <label class="block text-grey-darker text-sm font-bold mb-2" for="email">Email</label>
         <input v-model="form.email" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight" id="email" type="email" placeholder="Your Email">
       </div>
       <div class="mb-6">
-        <label class="block text-grey-darker text-sm font-bold mb-2" for="email">
-            Email
-          </label>
-        <textarea v-model="form.message" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3 leading-normal" name="message" rows="8" cols="80"></textarea>
+        <label class="block text-grey-darker text-sm font-bold mb-2" for="email">Message</label>
+        <textarea v-model="form.message" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3 leading-normal" name="message" rows="8" cols="80" placeholder="Write A Message..."></textarea>
       </div>
       <div class="flex items-center justify-between">
         <button type="submit" @click.prevent="handleSubmit" class="ml-auto bg-grey-darker hover:bg-grey-darkest text-white font-bold py-2 px-4 rounded">
-            Send
-          </button>
+          Send
+        </button>
       </div>
     </form>
   </div>
