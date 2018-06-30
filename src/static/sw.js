a4b6a008912ffb744229013052dc62a3bc5bdb73
David Royer <droyer01@gmail.com>
2018-06-30 02:52:58 -0400

    fixed fontawesome error, removed unused pics from static

--- src/static/sw.js
+++ src/static/sw.js
@@ -9,39 +9,39 @@ const workboxSW = new self.WorkboxSW({
 workboxSW.precache([
   {
     "url": "/_nuxt/app.91de370ac0d9d11b9c4892e1f088729f.css",
-    "revision": "1eaa397eb1172ca282d5a70b4e464eb3"
+    "revision": "bdbb9d8fb04afd60d1a0d16fdf30312e"
   },
   {
-    "url": "/_nuxt/app.9d8dc512d626a3490eb2.js",
-    "revision": "3e29eeeb775fdfcbc49560eae133d40b"
+    "url": "/_nuxt/app.f9f9214a9f05c986189a.js",
+    "revision": "4b2caf11df86c8b70e2462599548d3f8"
   },
   {
     "url": "/_nuxt/layouts/default.f2619d6155c66650fa2f.js",
     "revision": "b9b7916bc937ee719db6001e4a05da7c"
   },
   {
-    "url": "/_nuxt/manifest.ac686d85c24825348b73.js",
-    "revision": "45c35424121cc3656180940ae136268a"
+    "url": "/_nuxt/manifest.ba040cd2886f79ba8ca4.js",
+    "revision": "cb773aa302eeeeaf080bfc8bbd00f2e0"
   },
   {
-    "url": "/_nuxt/pages/about.7716f4a7669f51c3fdf7.js",
-    "revision": "7d3066537256a50b27d8ef9796da05ea"
+    "url": "/_nuxt/pages/about.c841cad22c158faeb270.js",
+    "revision": "900802efe9a9b24277c31df52fff313e"
   },
   {
-    "url": "/_nuxt/pages/blog.323ba46124a7bd14f3ab.js",
-    "revision": "a08d2324eb72c754a89f9e9943d8884b"
+    "url": "/_nuxt/pages/blog.21f4a5fdc1b6e8a6ba17.js",
+    "revision": "2e7b07c413a6b0ed40b875b64f79841b"
   },
   {
-    "url": "/_nuxt/pages/contact.dcbb72c2ad4f91e223e2.js",
-    "revision": "ccb4d6dd68f8f0cf19c187f1242b1080"
+    "url": "/_nuxt/pages/contact.3814345cfc63a21bcb43.js",
+    "revision": "5ec94a6fcf6a3f9abc59386cd5e78f1e"
   },
   {
-    "url": "/_nuxt/pages/examples.47a100d5c1e2a299419d.js",
-    "revision": "849a74164299eca9cad92cb75c7f7b44"
+    "url": "/_nuxt/pages/examples.dfa56b6000a59feff7a1.js",
+    "revision": "c54b3c1cee78d4191c51a1093330053c"
   },
   {
-    "url": "/_nuxt/pages/index.5a4e958ebd0e890a01cd.js",
-    "revision": "c199fbeb482794a55fb18c427c8d2a62"
+    "url": "/_nuxt/pages/index.850492c951e8d4d1f952.js",
+    "revision": "95c54f5d2e2dc633a670401df31faeca"
   },
   {
     "url": "/_nuxt/vendor.175e1c9039651bff9cc8.js",
